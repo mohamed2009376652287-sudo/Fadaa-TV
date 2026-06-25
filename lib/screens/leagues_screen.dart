392bc54b801ca7846fee64969ce805d4245b445f
mohamed2009376652287-sudo <mohamed2009376652.287@gmail.com>
2026-06-26 00:27:03 +0300

    Create leagues_screen.dart

---
+++ lib/screens/leagues_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class LeaguesScreen extends StatelessWidget {
+  const LeaguesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: const Text('البطولات', style: TextStyle(color: Colors.white)),
+        backgroundColor: Colors.black,
+      ),
+      body: const Center(
+        child: Text('قائمة البطولات ستظهر هنا', style: TextStyle(color: Colors.white)),
+      ),
+    );
+  }
+}
